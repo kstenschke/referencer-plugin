ffa09031b0bed726d3c3386e083263eba290d6a2
kstenschke <info@stenschke.com>
2017-06-02 23:38:52 +0200

    moved settings component to namespace root

--- src/com/kstenschke/referencer/ReferencerSettingsComponent.java
+++ src/com/kstenschke/referencer/ReferencerSettingsComponent.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.kstenschke.referencer.resources.ui;
+package com.kstenschke.referencer;
 
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.options.Configurable;
